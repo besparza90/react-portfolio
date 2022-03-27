866ff1cc4ac269ff4c2079d1727614eadf12310a
besparza <esparzabrian90@gmail.com>
2022-03-27 16:07:40 -0700

    testing code

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,9 +4,9 @@ const Resume = () => {
 
     return (
         <section className='my-5 text-background'>
-            <a href="https://besparza90.github.io/react-portfolio/build/static/media/reactresume.pdf" download>
-             <p>Please download my resume</p>
-             </a>
+            <a href="https://besparza90.github.io/react-portfolio//build/static/media/reactresume.pdf" target="_blank" rel= "noreferrer">
+            Download my resume
+        </a>{" "}
         </section>
     )
 }
